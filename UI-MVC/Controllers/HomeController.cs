60d5751c66ebf77bbc9038e86475331841949bd8
DenDrummer <muzikant96@gmail.com>
2017-02-16 17:30:32 +0100

    Add project files.

---
+++ UI-MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SC.UI.Web.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "SupportCenter demo application";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Informatie over KdG";
+
+            return View();
+        }
+    }
+}
